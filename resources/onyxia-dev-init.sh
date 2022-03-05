67e1930b2b16202926f448ac2b4a72d699d85efd
Joseph Garrone <joseph.garrone.gj@gmail.com>
2022-03-05 12:46:12 +0100

    Update onyxia-dev-init.sh

--- resources/onyxia-dev-init.sh
+++ resources/onyxia-dev-init.sh
@@ -1,15 +1,19 @@
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
+sudo apt-get install python2 #Only for scss (will be removed soon)
 sudo npm install -g yarn
 code-server --install-extension vscodevim.vim
+code-server --install-extension vscode-icons-team.vscode-icons
+
+git clone https://github.com/garronej/working_environnement && cd working_environnement && sudo ./apply.sh && source ~/.bashrc
+
 cd /home/coder/work
-echo "test2" > hello4.txt
-git clone https://github.com/garronej/tss-react     && cd tss-react                   && yarn        && yarn build             && cd ..
-git clone https://github.com/garronej/powerhooks    && cd powerhooks                  && yarn        && yarn build             && cd ..
-git clone https://github.com/InseeFrLab/onyxia-ui   && cd onyxia-ui                   && yarn        && yarn build             && cd ..
-git clone https://github.com/InseeFrLab/keycloakify && cd keycloakify                 && yarn        && yarn build             && cd ..
-git clone https://github.com/garronej/evt           && cd evt && git checkout v2_beta && npm install && npm run build          && cd ..
-git clone https://github.com/garronej/cra-envs      && cd cra-envs                    && yarn        && yarn build             && cd ..
-git clone https://github.com/garronej/tsafe         && cd tsafe                       && yarn        && yarn build             && cd ..
-git clone https://github.com/garronej/clean-redux   && cd clean-redux                 && yarn        && yarn build             && cd ..
-git clone https://github.com/InseeFrLab/onyxia-web  && cd onyxia-web                  && yarn        && yarn link_inhouse_deps && cd ..
+# git clone https://github.com/garronej/tss-react                && cd tss-react                   && yarn install --frozen-lockfile && yarn build    && cd ..
+# git clone https://github.com/garronej/powerhooks               && cd powerhooks                  && yarn install --frozen-lockfile && yarn build    && cd ..
+git clone https://github.com/InseeFrLab/onyxia-ui              && cd onyxia-ui                   && yarn install --frozen-lockfile && yarn build    && cd ..
+# git clone https://github.com/InseeFrLab/keycloakify            && cd keycloakify                 && yarn install --frozen-lockfile && yarn build    && cd ..
+# git clone https://github.com/garronej/evt                      && cd evt && git checkout v2_beta && npm ci                         && npm run build && cd ..
+# git clone https://github.com/etalab/cra-envs                   && cd cra-envs                    && yarn install --frozen-lockfile && yarn build    && cd ..
+# git clone https://github.com/garronej/tsafe                    && cd tsafe                       && yarn install --frozen-lockfile && yarn build    && cd ..
+# git clone https://github.com/garronej/redux-clean-architecture && cd redux-clean-architecture    && yarn install --frozen-lockfile && yarn build    && cd ..
+git clone https://github.com/InseeFrLab/onyxia-web             && cd onyxia-web                  && yarn install --frozen-lockfile                  && cd ..
