dd2a749abc5a719afb572052cd9079961b738620
Joseph Garrone <joseph.garrone.gj@gmail.com>
2022-03-05 12:05:16 +0100

    Update onyxia-dev-init.sh

--- resources/onyxia-dev-init.sh
+++ resources/onyxia-dev-init.sh
@@ -3,13 +3,13 @@ sudo apt-get install -y nodejs
 sudo npm install -g yarn
 code-server --install-extension vscodevim.vim
 cd /home/coder/work
-echo "test" > hello4.txt
-git clone tss-react   && cd tss-react   && yarn        && yarn build
-git clone powerhooks  && cd powerhooks  && yarn        && yarn build
-git clone onyxia-ui   && cd onyxia-ui   && yarn        && yarn build
-git clone keycloakify && cd keycloakify && yarn        && yarn build
-git clone evt         && cd evt         && npm install && npm run build
-git clone cra-envs    && cd cra-envs    && yarn        && yarn build
-git clone tsafe       && cd tsafe       && yarn        && yarn build
-git clone clean-redux && cd clean-redux && yarn        && yarn build
-git clone onyxia-web  && cd onyxia-web  && yarn        && yarn link_inhouse_deps
+echo "test2" > hello4.txt
+git clone https://github.com/garronej/tss-react     && cd tss-react                   && yarn        && yarn build             && cd ..
+git clone https://github.com/garronej/powerhooks    && cd powerhooks                  && yarn        && yarn build             && cd ..
+git clone https://github.com/InseeFrLab/onyxia-ui   && cd onyxia-ui                   && yarn        && yarn build             && cd ..
+git clone https://github.com/InseeFrLab/keycloakify && cd keycloakify                 && yarn        && yarn build             && cd ..
+git clone https://github.com/garronej/evt           && cd evt && git checkout v2_beta && npm install && npm run build          && cd ..
+git clone https://github.com/garronej/cra-envs      && cd cra-envs                    && yarn        && yarn build             && cd ..
+git clone https://github.com/garronej/tsafe         && cd tsafe                       && yarn        && yarn build             && cd ..
+git clone https://github.com/garronej/clean-redux   && cd clean-redux                 && yarn        && yarn build             && cd ..
+git clone https://github.com/InseeFrLab/onyxia-web  && cd onyxia-web                  && yarn        && yarn link_inhouse_deps && cd ..
