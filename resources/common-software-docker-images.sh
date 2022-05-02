6d1a23284fafcd60e66b4f7a61201c6fcbc60608
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2022-05-02 14:00:59 +0200

    Update common-software-docker-images.sh

--- resources/common-software-docker-images.sh
+++ resources/common-software-docker-images.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 # Utilities
-apt-get -y update && \
-    apt-get -y install wget \
-                       curl \
-                       jq \
-                       bash-completion \
-                       vim \
-                       unzip \
-                       ca-certificates-java && \
-    rm -rf /var/lib/apt/lists/*
+apt-get -y install wget \
+                   curl \
+                   jq \
+                   bash-completion \
+                   vim \
+                   unzip \
+                   ca-certificates-java
 
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && \
